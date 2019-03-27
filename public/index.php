8a4d40254070d5a3e74861d4e7d698f26538a801
Jeferson Kaefer <jefersonkaefer@gmail.com>
2019-03-27 17:18:51 +0000

    add verify contentType JSON request

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
@@ -19,9 +20,21 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ??
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+if ($request->headers->get('content-type') != 'application/json') {
+    $response = new Response();
+
+    $response->setContent('You need an API client with support JSON. :)');
+    $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+    // sets a HTTP response header
+    $response->headers->set('Content-Type', 'text/html');
+
+    // prints the HTTP headers followed by the content
+    $response->send();
+    $kernel->terminate($request, $response);
+}
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
